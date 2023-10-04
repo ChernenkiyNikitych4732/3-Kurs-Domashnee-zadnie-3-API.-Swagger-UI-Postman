0025898b152da023df6c4b778ef52fe23360d36e
Nikita Chernenkiy <chernenkiy.nikitych136@yandex.ru>
2023-10-04 12:23:00 +0200

    3 Курс Домашнее задание №30 Создание API Swagger-UI, Postman - изначальный комментарий

---
+++ src/main/java/sky/pro/course3/homework/Kurs3DomashneeZadanie3ApiSwaggerUiPostmanApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course3.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs3DomashneeZadanie3ApiSwaggerUiPostmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs3DomashneeZadanie3ApiSwaggerUiPostmanApplication.class, args);
+    }
+
+}
